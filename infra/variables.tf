8154e039da00b329c798a5177d1499a9954bf8f3
Lukas Zainzinger <lukas.zainzinger@gmail.com>
2025-10-11 16:59:33 +0200

    Feature/infrastructure (#2)
    * feat(infra): initial infra deployment with k8s and kafka
    
    * feat(infra): deploy kafka secrets to kubernetes
    
    * fix(infra): ignoring kubeconfig file

---
+++ infra/variables.tf
@@ -0,0 +1,36 @@
+
+variable "exoscale_zone" {
+  description = "The Exoscale zone to deploy resources in."
+  type        = string
+  default     = "at-vie-1"
+}
+
+variable "sks_cluster_name" {
+  description = "The name of the SKS cluster."
+  type        = string
+  default     = "playpal-sks-cluster"
+}
+
+variable "sks_version" {
+  description = "The Kubernetes version for the SKS cluster."
+  type        = string
+  default     = "1.34.1"
+}
+
+variable "kafka_service_name" {
+  description = "The name of the DBaaS Kafka service."
+  type        = string
+  default     = "playpal-kafka-service"
+}
+
+variable "kafka_version" {
+  description = "The version for the DBaaS Kafka service. Recent versions use Kraft."
+  type        = string
+  default     = "3.8"
+}
+
+variable "kafka_plan" {
+  description = "The service plan on Exoscale for DBaaS Kafka."
+  type = string
+  default = "startup-2"
+}
