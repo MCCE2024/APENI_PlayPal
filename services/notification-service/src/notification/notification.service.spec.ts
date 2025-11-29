539ae0dd325326e364ad31112e2d1086a6291762
Marco Nika <112717705+msauhammel@users.noreply.github.com>
2025-11-29 21:10:47 +0100

    feat: Add workflow (build,test,lint) and Dockerfile (#23)

--- services/notification-service/src/notification/notification.service.spec.ts
+++ services/notification-service/src/notification/notification.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationService } from './notification.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('NotificationService', () => {
   let service: NotificationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationService],
+      providers: [
+        NotificationService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<NotificationService>(NotificationService);
