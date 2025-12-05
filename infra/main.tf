966bd370e43f13b9204038abe7844d2f28b4c4b3
lzainzinger <lukas.zainzinger@gmail.com>
2025-12-05 18:05:00 +0100

    fix tf local

--- infra/main.tf
+++ infra/main.tf
@@ -8,7 +8,7 @@ terraform {
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    #skip_requesting_account_id  = true
+    skip_requesting_account_id  = true
   }
   required_providers {
     exoscale = {
