d4fcd59b9d2ad4d9c8a458ec733420b5f8394215
lzainzinger <lukas.zainzinger@gmail.com>
2026-01-17 12:15:00 +0100

    fix: listening ports

--- services/matching-service/src/main.ts
+++ services/matching-service/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3005);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 void bootstrap();
--- services/notification-service/src/main.ts
+++ services/notification-service/src/main.ts
@@ -38,6 +38,6 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
-  await app.listen(process.env.PORT ?? 3006);
+  await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
