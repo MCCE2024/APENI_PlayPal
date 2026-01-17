fdc9c195b408bae804b45dabbbdc07529e6ffcce
lzainzinger <lukas.zainzinger@gmail.com>
2026-01-17 10:22:43 +0100

    split up infra and deployment pipeline

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,9 @@
-
 output "sks_cluster_endpoint" {
   description = "The endpoint of the SKS cluster."
   value       = exoscale_sks_cluster.prod_cluster.endpoint
 }
 
+output "kubeconfig" {
+  value     = exoscale_sks_kubeconfig.prod_cluster_kubeconfig.kubeconfig
+  sensitive = true
+}
