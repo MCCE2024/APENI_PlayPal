306bcfe24d2491ffdbe240c16e28dfaffbc41247
lzainzinger <lukas.zainzinger@gmail.com>
2026-01-18 13:08:59 +0100

    fix: add root controller to matching service and wait for rollout in pipeline

--- services/matching-service/src/app.module.ts
+++ services/matching-service/src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ScheduleModule } from '@nestjs/schedule'; // Import this
+import { ScheduleModule } from '@nestjs/schedule';
 import { MatchingModule } from './matching/matching.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ScheduleModule.forRoot(), // Add this
+    ScheduleModule.forRoot(),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -19,5 +21,7 @@ import { MatchingModule } from './matching/matching.module';
     }),
     MatchingModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
